c75bc0f9843a904d47651ef2859d93777f9e3f81
akrauss419 <akrauss419@gmail.com>
2022-12-16 09:49:05 -0800

    Shops List Page complete

--- src/pages/ShopsListPage/ShopsListPage.jsx
+++ src/pages/ShopsListPage/ShopsListPage.jsx
@@ -2,6 +2,13 @@ import ShopCard from '../../components/ShopCard/ShopCard';
 
 export default function ShopsListPage({ shops }) {
   return(
-    <h1>Explore Donut Shops</h1>
+    <>
+      <h1>Explore Donut Shops</h1>
+      <div>
+        {shops.map((s, idx) => {
+          return <ShopCard shop={s} key={idx} />;
+        })}
+      </div>
+    </>
   );
 }
