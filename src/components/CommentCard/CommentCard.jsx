9be8a5e4457161a578671348c49f0e4bd9682951
akrauss419 <akrauss419@gmail.com>
2022-12-16 22:09:57 -0800

    DonutCard updates

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,7 +1,13 @@
 export default function CommentCard({ comment }) {
+  const date = new Date(comment.createdAt);
+  const dateOptions = {year: 'numeric', month: 'short', day: 'numeric'};
+  
   return(
     <div>
-      {comment.content}
+      <div>
+        {comment.content}
+      </div>
+      <p>Posted by {comment.user} at {date.toLocaleDateString(undefined, dateOptions)}</p>
     </div>
   );
 }
